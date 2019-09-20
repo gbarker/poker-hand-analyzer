e0ce04b4a1f79b9b60bdc1d600f268a99a23a7a5
George Barker <george_1776@yahoo.com>
2019-09-20 09:27:09 -0400

    Refactor check point...

--- poker.hs
+++ poker.hs
@@ -7,27 +7,73 @@ card r s = (r, s)
 
 deck = [card r s | r <- ranks, s <- suits]
 
+-- Some constants
+highCard = "High Card"
+highCardValue = 0
+onePair = "One Pair"
+onePairValue =  67108864 
+twoPair = "Two Pair"
+twoPairValue =  134217728 
+threeOfAKind = "Three of a Kind"
+threeOfAKindValue =  268435456 
+straight = "Straight"
+straightValue =  536870912 
+flush = "Flush"
+flushValue =  1073741824
+fullHouse = "Full House"
+fullHouseValue =  2147483648 
+fourOfAKind = "Four of a Kind"
+fourOfAKindValue =  4294967296 
+straightFlush = "Straight Flush"
+straightFlushValue =  8589934592 
+
+--handTypes = [highCard, onePair, twoPair, threeOfAKind, straight, flush, fullHouse, fourOfAKind, straightFlush]
+
+-- Use a Sum-free sequence for ranking cards: https://en.wikipedia.org/wiki/Sum-free_sequence
+cardRankValueMap = zip ranks [2^n | n <- [0..12]]
+pairedCardRankValueMap = zip ranks [2^n | n <- [13..25]]
+--handTypeValueMap = [2^n | n <- [26..34]]
+
+wheelCardValueSum = 4111
+straightCardValueSums = [wheelCardValueSum, 31, 62, 124, 248, 496, 992, 1984, 3968, 7936]
 
 -- Hand identification
-handIdentification :: [(Int, Int)] -> (String, Int, Integer)
+handIdentification :: [(Int, Int)] -> (String, Integer)
 handIdentification cards@[a,b,c,d,e] 
-    | countUniqueSuits cards == 1 && isWheel cards == True = ("Straight Flush", 8, 3)
-    | countUniqueSuits cards == 1 && isStraight cards == True = ("Straight Flush", 8, fromIntegral (maximum (getRanks cards)))
-    | isQuads cards == True = ("Quads", 7, fromIntegral (100 * (fst (getQuadAndKickerRank cards) + 1) + snd (getQuadAndKickerRank cards)))
-    | hasSet cards == True && hasExactlyOnePair cards == True = ("Boat", 6, fromIntegral (100 * (fst (getBoatAndKickerRank cards) + 1) + snd (getBoatAndKickerRank cards)))
-    | countUniqueSuits cards == 1 = ("Flush", 5, sumByRank(cards))
-    | isWheel cards == True = ("Straight", 4, 3)
-    | isStraight cards == True = ("Straight", 4, fromIntegral (maximum (getRanks cards)))
-    | hasSet cards == True = ("Trips", 3, getTripsValue cards)
-    | hasExactlyTwoPair cards == True = ("Two Pair", 2, getTwoPairValue cards)
-    | hasExactlyOnePair cards == True = ("One Pair", 1, getOnePairValue cards)
-    | otherwise = ("High Card", 0, sumByRank(cards))
+    | isAFlush == True && isAStraight == True = (straightFlush, straightFlushValue + getStraightKickerValue cards sumOfCardValues)
+    | isQuads cards == True = (fourOfAKind, fourOfAKindValue + 0)
+    | hasSet cards == True && hasExactlyOnePair cards == True = (fullHouse, fullHouseValue + 0)
+    | isAFlush == True = (flush, flushValue + sumOfCardValues)
+    | isAStraight == True = (straight, straightValue + getStraightKickerValue cards sumOfCardValues)
+    | hasSet cards == True = (threeOfAKind, 0)
+    | hasExactlyTwoPair cards == True = (twoPair, 0)
+    | hasExactlyOnePair cards == True = (onePair, 0)
+    | otherwise = (highCard, highCardValue + sumOfCardValues)
+    where   sumOfCardValues= sumCardValues(cards)
+            isAFlush = isFlush cards
+            isAStraight = isStraight sumOfCardValues
 
 -- Error: wrong number of cards
-handIdentification (x:xs) = ("There are five cards in a poker hand...you have supplied " ++ show (length (x:xs)), 0, 0)
+handIdentification (x:xs) = ("There are five cards in a poker hand...you have supplied " ++ show (length (x:xs)), 0)
 
 -- Hand Identification supporting functions:
 
+isFlush :: [(Int, Int)] -> Bool
+isFlush (x:[]) = True
+isFlush ((_, s):xs) = if s == head [suit | (rank, suit) <- xs] then isFlush xs else False
+
+isStraight :: Integer -> Bool
+isStraight sumOfCardValues = elem sumOfCardValues straightCardValueSums
+
+getStraightKickerValue :: [(Int, Int)] -> Integer -> Integer
+getStraightKickerValue cards sumOfCardValues = fromIntegral (if sumOfCardValues == wheelCardValueSum then 5 else maximum (getRanks cards))
+
+-- todo: this can be optimized...right now it's scanning all 5 cards one per card...some kind of running count that stops when we hit four for a single rank?
+isQuads :: [(Int, Int)] -> Bool
+isQuads cards = length [r | (r, _) <- cards, countCardsWithRank cards r == 4] == 4
+
+
+            
 countUniqueSuits :: [(Int, Int)] -> Int
 countUniqueSuits [] = 0
 countUniqueSuits ((r, s):xs) = (if elem s (getSuits xs) then 0 else 1) + countUniqueSuits xs
@@ -55,7 +101,7 @@ hasSet cards = length [r | (r, _) <- cards, countCardsWithRank cards r == 3] == 
 getTripsValue :: [(Int, Int)] -> Integer
 getTripsValue cards = fromIntegral ((setRank + 1) * 10000000000000) + kickersValue
     where   setRank = getSetRank cards
-            kickersValue = sumByRank [(r, s) | (r, s) <- cards, r /= setRank]
+            kickersValue = sumCardValues [(r, s) | (r, s) <- cards, r /= setRank]
 
 getTwoPairValue :: [(Int, Int)] -> Integer
 getTwoPairValue cards = fromIntegral ((maximum pairRanks + 1) * 10000) + fromIntegral ((minimum pairRanks + 1) * 100) + fromIntegral kickerRank
@@ -65,10 +111,7 @@ getTwoPairValue cards = fromIntegral ((maximum pairRanks + 1) * 10000) + fromInt
 getOnePairValue :: [(Int, Int)] -> Integer
 getOnePairValue cards = fromIntegral ((pairRank + 1) * 10000000000000) + kickersValue
     where   pairRank = head (getPairRanks cards)
-            kickersValue = sumByRank [(r, s) | (r, s) <- cards, r /= pairRank]
-
-isQuads :: [(Int, Int)] -> Bool
-isQuads cards = length [r | (r, _) <- cards, countCardsWithRank cards r == 4] == 4
+            kickersValue = sumCardValues [(r, s) | (r, s) <- cards, r /= pairRank]
 
 hasExactlyOnePair :: [(Int, Int)] -> Bool
 hasExactlyOnePair cards = length [r | (r, _) <- cards, countCardsWithRank cards r == 2] == 2
@@ -85,19 +128,8 @@ getPairRanks cards = if minPair == maxPair then [minPair] else [minPair, maxPair
             minPair = minimum pairs
             maxPair = maximum pairs
 
-sumByRank :: [(Int, Int)] -> Integer
-sumByRank cards = sum [getCardRankMultiplier(card) | card <- cards]
-
-isStraight :: [(Int, Int)] -> Bool
-isStraight cards
-    | isWheel cards = True
-    | otherwise = if elem (minRank + 4) ranks &&  elem (minRank + 3) ranks &&  elem (minRank + 2) ranks &&  elem (minRank + 1) ranks then True else False
-    where   ranks = getRanks cards
-            minRank = minimum ranks
-
-isWheel :: [(Int, Int)] -> Bool
-isWheel cards = if elem 12 ranks && elem 0 ranks && elem 1 ranks && elem 2 ranks && elem 3 ranks then True else False
-    where ranks = getRanks cards
+sumCardValues :: [(Int, Int)] -> Integer
+sumCardValues cards = sum [getCardRankValue(card) | card <- cards]
 
 getRanks cards = [r | (r, s) <- cards]
 
@@ -105,30 +137,31 @@ getSuits cards = [s | (r, s) <- cards]
 
 highCards cards = [x | x <- cards, fst x == maximum [fst y | y <- cards]]
 
-cardRankValueMap = zip ranks [1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000, 10000000000, 100000000000, 1000000000000]
+getCardRankValue :: (Int, Int) -> Integer
+getCardRankValue (rankToMatch, _) = getRankValue rankToMatch
 
-getCardRankMultiplier :: (Int, Int) -> Integer
-getCardRankMultiplier (rankToMatch, _) = getRankMultiplier rankToMatch
+getRankValue :: Int -> Integer
+getRankValue rankToMatch = head [x | (r, x) <- cardRankValueMap, r == rankToMatch]
 
-getRankMultiplier :: Int -> Integer
-getRankMultiplier rankToMatch = head [x | (r, x) <- cardRankValueMap, r == rankToMatch]
+--getHandTypeValue :: String -> Integer
+--getHandTypeValue typeToMatch = head [x | (t, x <- handTypeValueMap, t == typeToMatch)]
 
 -- Hand Identification Tests:
-testStraightFlush = handIdentification [(5, 2), (6, 2), (7, 2), (8, 2), (9, 2)] == ("Straight Flush", 8, 9) && handIdentification [(3, 3), (1, 3), (2, 3), (12, 3), (0, 3)] == ("Straight Flush", 8, 3)
-testQuads = handIdentification [(0, 1), (0, 2), (0, 3), (0, 4), (1, 1)] == ("Quads", 7, 101)
-testBoat = handIdentification [(0, 1), (0, 2), (0, 3), (1, 1), (1, 2)] == ("Boat", 6, 101)
-testFlush = handIdentification [(5, 1), (1, 1), (9, 1), (12, 1), (7, 1)] == ("Flush", 5, 1001010100010)
-testStraight = handIdentification [(5, 0), (6, 1), (7, 2), (8, 2), (9, 2)] == ("Straight", 4, 9)
-testTrips = handIdentification [(5, 0), (5, 1), (5, 2), (8, 2), (9, 2)] == ("Trips", 3, 60001100000000)
-testTwoPair = handIdentification [(5, 0), (5, 1), (4, 2), (4, 2), (9, 2)] == ("Two Pair", 2, 60509)
-testOnePair = handIdentification [(5, 0), (5, 1), (4, 2), (3, 2), (9, 2)] == ("One Pair", 1, 60001000011000)
-testHighCard = handIdentification [(8, 2), (1, 1), (9, 2), (11, 0), (6, 0)] == ("High Card", 0, 101101000010)
+testStraightFlush = handIdentification [(5, 2), (6, 2), (7, 2), (8, 2), (9, 2)] == ("Straight Flush",  8589934592 + 512) && handIdentification [(3, 3), (1, 3), (2, 3), (12, 3), (0, 3)] == ("Straight Flush",  8589934592 + 8)
+testQuads = handIdentification [(0, 1), (0, 2), (0, 3), (0, 4), (1, 1)] == ("Quads", 101)
+testBoat = handIdentification [(0, 1), (0, 2), (0, 3), (1, 1), (1, 2)] == ("Boat", 101)
+testFlush = handIdentification [(5, 1), (1, 1), (9, 1), (12, 1), (7, 1)] == ("Flush", 1001010100010)
+testStraight = handIdentification [(5, 0), (6, 1), (7, 2), (8, 2), (9, 2)] == ("Straight", 9)
+testTrips = handIdentification [(5, 0), (5, 1), (5, 2), (8, 2), (9, 2)] == ("Trips", 60001100000000)
+testTwoPair = handIdentification [(5, 0), (5, 1), (4, 2), (4, 2), (9, 2)] == ("Two Pair", 60509)
+testOnePair = handIdentification [(5, 0), (5, 1), (4, 2), (3, 2), (9, 2)] == ("One Pair", 60001000011000)
+testHighCard = handIdentification [(8, 2), (1, 1), (9, 2), (11, 0), (6, 0)] == ("High Card", 101101000010)
 
-testTooManyCards = handIdentification [(0,0), (1,0), (2,0), (3,0), (4,0), (5, 0)] == ("There are five cards in a poker hand...you have supplied 6", 0, 0)
-testTooFewCards = handIdentification [(0,0), (1,0), (2,0), (3,0)] == ("There are five cards in a poker hand...you have supplied 4", 0, 0)
+testTooManyCards = handIdentification [(0,0), (1,0), (2,0), (3,0), (4,0), (5, 0)] == ("There are five cards in a poker hand...you have supplied 6", 0)
+testTooFewCards = handIdentification [(0,0), (1,0), (2,0), (3,0)] == ("There are five cards in a poker hand...you have supplied 4", 0)
 
 testCountSuits = countUniqueSuits [(1,0),(1,0),(1,0),(1,0),(2,3)] == 2
-testIsStraight = isStraight [(1,0),(1,0),(1,0),(1,0),(2,3)] == False && isStraight [(4, 0), (7, 0), (3, 1), (5, 2), (6, 3)] == True && isStraight [(12, 0), (0, 0), (1, 1), (2, 2), (3, 3)] == True
+testIsStraight = isStraight (sumCardValues [(1,0),(1,0),(1,0),(1,0),(2,3)]) == False && isStraight (sumCardValues [(4, 0), (7, 0), (3, 1), (5, 2), (6, 3)]) == True && isStraight (sumCardValues [(12, 0), (0, 0), (1, 1), (2, 2), (3, 3)]) == True
 testHighCards = highCards [(0,0),(0,1),(1,1),(1,2)] == [(1,1),(1,2)]
 
 
