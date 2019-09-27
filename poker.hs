c1572a5f8a14ee2152bc92f224f53b184b092e30
George Barker <george_1776@yahoo.com>
2019-09-27 12:16:51 -0400

    refactor

--- poker.hs
+++ poker.hs
@@ -2,12 +2,13 @@
 ranks = [0..12]
 suits = [0..3]
 
-card :: Int -> Int -> (Int, Int)
-card r s = (r, s)
+-- card :: Int -> Int -> (Int, Int)
+-- card r s = (r, s)
 
-deck = [card r s | r <- ranks, s <- suits]
+deck = [(r, s) | r <- ranks, s <- suits]
 
--- Some constants
+
+-- Constants
 highCard = "High Card"
 highCardValue = 0
 onePair = "One Pair"
@@ -27,27 +28,26 @@ fourOfAKindValue =  4294967296
 straightFlush = "Straight Flush"
 straightFlushValue =  8589934592 
 
---handTypes = [highCard, onePair, twoPair, threeOfAKind, straight, flush, fullHouse, fourOfAKind, straightFlush]
-
 -- Use a Sum-free sequence for ranking cards: https://en.wikipedia.org/wiki/Sum-free_sequence
 cardRankValueMap = zip ranks [2^n | n <- [0..12]]
 pairedCardRankValueMap = zip ranks [2^n | n <- [13..25]]
---handTypeValueMap = [2^n | n <- [26..34]]
 
 wheelCardValueSum = 4111
+wheelCardKickerValue = 8
 straightCardValueSums = [wheelCardValueSum, 31, 62, 124, 248, 496, 992, 1984, 3968, 7936]
 
+
 -- Hand identification
-handIdentification :: [(Int, Int)] -> (String, Integer)
+handIdentification :: [(Int, Int)] -> (String, Int)
 handIdentification cards@[a,b,c,d,e] 
     | isAFlush == True && isAStraight == True = (straightFlush, straightFlushValue + getStraightKickerValue cards sumOfCardValues)
     | isQuads rankCounts == True = (fourOfAKind, getQuadValue rankCounts)
-    | hasSet cards == True && hasExactlyOnePair cards == True = (fullHouse, getBoatValue cards)
+    | isBoat rankCounts == True = (fullHouse, getBoatValue rankCounts)
     | isAFlush == True = (flush, flushValue + sumOfCardValues)
     | isAStraight == True = (straight, straightValue + getStraightKickerValue cards sumOfCardValues)
-    | hasSet cards == True = (threeOfAKind, 0)
-    | hasExactlyTwoPair cards == True = (twoPair, 0)
-    | hasExactlyOnePair cards == True = (onePair, 0)
+    | isSet rankCounts == True = (threeOfAKind, getSetValue rankCounts)
+    | isTwoPair rankCounts == True = (twoPair, getTwoPairValue rankCounts)
+    | isOnePair rankCounts == True = (onePair, getOnePairValue rankCounts)
     | otherwise = (highCard, highCardValue + sumOfCardValues)
     where   sumOfCardValues= sumCardValues(cards)
             isAFlush = isFlush cards
@@ -62,11 +62,11 @@ isFlush :: [(Int, Int)] -> Bool
 isFlush (x:[]) = True
 isFlush ((_, s):xs) = if s == head [suit | (rank, suit) <- xs] then isFlush xs else False
 
-isStraight :: Integer -> Bool
+isStraight :: Int -> Bool
 isStraight sumOfCardValues = elem sumOfCardValues straightCardValueSums
 
-getStraightKickerValue :: [(Int, Int)] -> Integer -> Integer
-getStraightKickerValue cards sumOfCardValues = fromIntegral (if sumOfCardValues == wheelCardValueSum then 3 else maximum (getRanks cards))
+getStraightKickerValue :: [(Int, Int)] -> Int -> Int
+getStraightKickerValue cards sumOfCardValues = fromIntegral (if sumOfCardValues == wheelCardValueSum then wheelCardKickerValue else getRankValue (maximum (getRanks cards)))
 
 groupByRank :: [(Int, Int)] -> [(Int, Int)]
 groupByRank cards = distinct [(r1, length [r | (r, _) <- cards, r == r1]) | (r1, _) <- cards]
@@ -78,131 +78,105 @@ distinct (x:xs) = x:distinct (filter ((/=) x) xs)
 
 isQuads :: [(Int, Int)] -> Bool
 isQuads rankCounts = if length rankCounts == 2 then (snd (head rankCounts)) `elem` [1, 4] else False
---isQuads cards@((firstRank, _):(secondRank, _):xs) = if countCardsWithRank cards firstRank == 4 || countCardsWithRank cards secondRank == 4 then True else False
 
-getQuadValue :: [(Int, Int)] -> Integer
+getQuadValue :: [(Int, Int)] -> Int
 getQuadValue rankCounts = fourOfAKindValue + getPairedRankValue quadRank + getRankValue kickerRank
     where   quadRank = head [r | (r, c) <- rankCounts, c == 4]
             kickerRank = head [r | (r, c) <- rankCounts, c == 1]
 
---getQuadValue cards@((firstRank, _):xs) = fourOfAKindValue + getPairedRankValue quadRank + getRankValue kickerRank
---    where   countOfFirstRank = countCardsWithRank cards firstRank
---            secondRank = head [x | (x, _) <- cards, x /= firstRank]
---            quadRank = if countOfFirstRank == 4 then firstRank else secondRank
---            kickerRank = if countOfFirstRank == 4 then secondRank else firstRank
+isBoat :: [(Int, Int)] -> Bool
+isBoat rankCounts = if length [c | (_, c) <- rankCounts, c == 3] == 1 && length [c | (_, c) <- rankCounts, c == 2] == 1 then True else False
+                        
+getBoatValue :: [(Int, Int)] -> Int
+getBoatValue rankCounts = fullHouseValue + getPairedRankValue setRank + getRankValue kickerRank
+    where   setRank = head [r | (r, c) <- rankCounts, c == 3]
+            kickerRank = head [r | (r, c) <- rankCounts, c == 2]
 
-getBoatValue :: [(Int, Int)] -> Integer
-getBoatValue cards@((firstRank, _):xs) = fullHouseValue + getPairedRankValue boatRank + getRankValue kickerRank
-    where   countOfFirstRank = countCardsWithRank cards firstRank
-            secondRank = head [x | (x, _) <- cards, x /= firstRank]
-            boatRank = if countOfFirstRank == 3 then firstRank else secondRank
-            kickerRank = if countOfFirstRank == 3 then secondRank else firstRank
-            
-            
-countUniqueSuits :: [(Int, Int)] -> Int
-countUniqueSuits [] = 0
-countUniqueSuits ((r, s):xs) = (if elem s (getSuits xs) then 0 else 1) + countUniqueSuits xs
+isSet :: [(Int, Int)] -> Bool
+isSet rankCounts = if length [c | (_, c) <- rankCounts, c == 3] == 1 && length [c | (_, c) <- rankCounts, c == 1] == 2 then True else False
 
-countUniqueRanks :: [(Int, Int)] -> Int
-countUniqueRanks [] = 0
-countUniqueRanks ((r, s):xs) = (if elem r (getRanks xs) then 0 else 1) + countUniqueRanks xs
+getSetValue :: [(Int, Int)] -> Int
+getSetValue rankCounts = threeOfAKindValue + getPairedRankValue setRank + valueOfKickers
+    where   setRank = head [r | (r, c) <- rankCounts, c == 3]
+            valueOfKickers = sum [getRankValue r | (r, c) <- rankCounts, c == 1]
 
-countCardsWithRank :: [(Int, Int)] -> Int -> Int
-countCardsWithRank cards rank = length [(r, s) | (r, s) <- cards, r == rank]
+isTwoPair :: [(Int, Int)] -> Bool
+isTwoPair rankCounts = if length [c | (_, c) <- rankCounts, c == 2] == 2 then True else False
 
-getBoatAndKickerRank :: [(Int, Int)] -> (Int, Int)
-getBoatAndKickerRank cards@((firstRank, _):xs) = if countOfFirstRank == 3 then (firstRank, secondRank) else (secondRank, firstRank)
-    where   countOfFirstRank = countCardsWithRank cards firstRank
-            secondRank = head [x | (x, _) <- cards, x /= firstRank]
+getTwoPairValue :: [(Int, Int)] -> Int
+getTwoPairValue rankCounts = twoPairValue + valueOfPairs + valueOfKickers
+    where   valueOfPairs = sum [getPairedRankValue r | (r, c) <- rankCounts, c == 2]
+            valueOfKickers = sum [getRankValue r | (r, c) <- rankCounts, c == 1]
 
-hasSet :: [(Int, Int)] -> Bool
-hasSet cards = length [r | (r, _) <- cards, countCardsWithRank cards r == 3] == 3
+isOnePair :: [(Int, Int)] -> Bool
+isOnePair rankCounts = if length [c | (_, c) <- rankCounts, c == 2] == 1 && length [c | (_, c) <- rankCounts, c == 1] == 3 then True else False
 
-getTripsValue :: [(Int, Int)] -> Integer
-getTripsValue cards = fromIntegral ((setRank + 1) * 10000000000000) + kickersValue
-    where   setRank = getSetRank cards
-            kickersValue = sumCardValues [(r, s) | (r, s) <- cards, r /= setRank]
-
-getTwoPairValue :: [(Int, Int)] -> Integer
-getTwoPairValue cards = fromIntegral ((maximum pairRanks + 1) * 10000) + fromIntegral ((minimum pairRanks + 1) * 100) + fromIntegral kickerRank
-    where   pairRanks = getPairRanks cards
-            kickerRank = head [r | (r, s) <- cards, elem r pairRanks /= True]
-
-getOnePairValue :: [(Int, Int)] -> Integer
-getOnePairValue cards = fromIntegral ((pairRank + 1) * 10000000000000) + kickersValue
-    where   pairRank = head (getPairRanks cards)
-            kickersValue = sumCardValues [(r, s) | (r, s) <- cards, r /= pairRank]
-
-hasExactlyOnePair :: [(Int, Int)] -> Bool
-hasExactlyOnePair cards = length [r | (r, _) <- cards, countCardsWithRank cards r == 2] == 2
-
-hasExactlyTwoPair :: [(Int, Int)] -> Bool
-hasExactlyTwoPair cards = length [r | (r, _) <- cards, countCardsWithRank cards r == 2] == 4
-
-getSetRank :: [(Int, Int)] -> Int
-getSetRank cards = head [r | (r, _) <- cards, countCardsWithRank cards r == 3]
-
-getPairRanks :: [(Int, Int)] -> [Int]
-getPairRanks cards = if minPair == maxPair then [minPair] else [minPair, maxPair]
-    where   pairs = [r | (r, _) <- cards, countCardsWithRank cards r == 2]
-            minPair = minimum pairs
-            maxPair = maximum pairs
-
-sumCardValues :: [(Int, Int)] -> Integer
+getOnePairValue :: [(Int, Int)] -> Int
+getOnePairValue rankCounts = onePairValue + getPairedRankValue pairRank + valueOfKickers
+    where   pairRank = head [r | (r, c) <- rankCounts, c == 2]
+            valueOfKickers = sum [getRankValue r | (r, c) <- rankCounts, c == 1]
+                                                                        
+sumCardValues :: [(Int, Int)] -> Int
 sumCardValues cards = sum [getCardRankValue(card) | card <- cards]
 
 getRanks cards = [r | (r, s) <- cards]
 
-getSuits cards = [s | (r, s) <- cards]
-
-highCards cards = [x | x <- cards, fst x == maximum [fst y | y <- cards]]
-
-getCardRankValue :: (Int, Int) -> Integer
+getCardRankValue :: (Int, Int) -> Int
 getCardRankValue (rankToMatch, _) = getRankValue rankToMatch
 
-getRankValue :: Int -> Integer
+getRankValue :: Int -> Int
 getRankValue rankToMatch = head [x | (r, x) <- cardRankValueMap, r == rankToMatch]
 
-getPairedRankValue :: Int -> Integer
+getPairedRankValue :: Int -> Int
 getPairedRankValue rankToMatch = head [x | (r, x) <- pairedCardRankValueMap, r == rankToMatch]
 
---getHandTypeValue :: String -> Integer
---getHandTypeValue typeToMatch = head [x | (t, x <- handTypeValueMap, t == typeToMatch)]
-
 -- Hand Identification Tests:
-testStraightFlush = handIdentification [(5, 2), (6, 2), (7, 2), (8, 2), (9, 2)] == ("Straight Flush",  8589934592 + 9) && handIdentification [(3, 3), (1, 3), (2, 3), (12, 3), (0, 3)] == ("Straight Flush",  8589934592 + 3)
-testQuads = handIdentification [(0, 1), (0, 2), (0, 3), (0, 4), (1, 1)] == ("Four of a Kind", 4294967296 +  8192 + 2)
-testBoat = handIdentification [(0, 1), (0, 2), (0, 3), (1, 1), (1, 2)] == ("Full House", 101)
-testFlush = handIdentification [(5, 1), (1, 1), (9, 1), (12, 1), (7, 1)] == ("Flush", 1001010100010)
-testStraight = handIdentification [(5, 0), (6, 1), (7, 2), (8, 2), (9, 2)] == ("Straight", 9)
-testTrips = handIdentification [(5, 0), (5, 1), (5, 2), (8, 2), (9, 2)] == ("Trips", 60001100000000)
-testTwoPair = handIdentification [(5, 0), (5, 1), (4, 2), (4, 2), (9, 2)] == ("Two Pair", 60509)
-testOnePair = handIdentification [(5, 0), (5, 1), (4, 2), (3, 2), (9, 2)] == ("One Pair", 60001000011000)
-testHighCard = handIdentification [(8, 2), (1, 1), (9, 2), (11, 0), (6, 0)] == ("High Card", 101101000010)
+testStraightFlush = handIdentification [(5, 2), (6, 2), (7, 2), (8, 2), (9, 2)] == (straightFlush,  straightFlushValue + 512)
+testWheelFlush = handIdentification [(3, 3), (1, 3), (2, 3), (12, 3), (0, 3)] == (straightFlush,  straightFlushValue + 8)
+testQuads = handIdentification [(0, 1), (0, 2), (0, 3), (0, 4), (1, 1)] == (fourOfAKind, fourOfAKindValue +  8192 + 2)
+testBoat = handIdentification [(0, 1), (0, 2), (0, 3), (1, 1), (1, 2)] == (fullHouse, fullHouseValue + 8192 + 2)
+testFlush = handIdentification [(5, 1), (1, 1), (9, 1), (12, 1), (7, 1)] == (flush, flushValue + 32 + 2 + 512 + 4096 + 128)
+testStraight = handIdentification [(5, 0), (6, 1), (7, 2), (8, 2), (9, 2)] == (straight, straightValue + 512)
+testWheel = handIdentification [(3, 0), (2, 1), (1, 2), (0, 2), (12, 2)] == (straight, straightValue + 8)
+testTrips = handIdentification [(5, 0), (5, 1), (5, 2), (8, 2), (9, 2)] == (threeOfAKind, threeOfAKindValue + 262144 + 256 + 512)
+testTwoPair = handIdentification [(5, 0), (5, 1), (4, 2), (4, 2), (9, 2)] == (twoPair, twoPairValue + 262144 + 131072 + 512)
+testOnePair = handIdentification [(5, 0), (5, 1), (4, 2), (3, 2), (9, 2)] == (onePair, onePairValue + 262144 + 16 + 8 + 512)
+testHighCard = handIdentification [(8, 2), (1, 1), (9, 2), (11, 0), (6, 0)] == (highCard, highCardValue + 256 + 2 + 512 + 2048 + 64)
 
-testTooManyCards = handIdentification [(0,0), (1,0), (2,0), (3,0), (4,0), (5, 0)] == ("There are five cards in a poker hand...you have supplied 6", 0)
-testTooFewCards = handIdentification [(0,0), (1,0), (2,0), (3,0)] == ("There are five cards in a poker hand...you have supplied 4", 0)
+testTooManyCards = handIdentification [(0,0), (1,0), (2,0), (3,0), (4,0), (5, 0)] == ("There are five cards in a poker hand...received 6", 0)
+testTooFewCards = handIdentification [(0,0), (1,0), (2,0), (3,0)] == ("There are five cards in a poker hand...received 4", 0)
 
-testCountSuits = countUniqueSuits [(1,0),(1,0),(1,0),(1,0),(2,3)] == 2
-testIsStraight = isStraight (sumCardValues [(1,0),(1,0),(1,0),(1,0),(2,3)]) == False && isStraight (sumCardValues [(4, 0), (7, 0), (3, 1), (5, 2), (6, 3)]) == True && isStraight (sumCardValues [(12, 0), (0, 0), (1, 1), (2, 2), (3, 3)]) == True
-testHighCards = highCards [(0,0),(0,1),(1,1),(1,2)] == [(1,1),(1,2)]
+testAllHandIdentification = testStraightFlush && testWheelFlush && testQuads && testBoat && testFlush && testStraight && testWheel && testTrips && testTwoPair && testOnePair && testHighCard && testTooManyCards && testTooFewCards
 
 
 -- Convert to strings for presentation
 rankStringMap = zip ranks "23456789TJQKA"
 suitStringMap = zip suits "CDHS"
 
-cardString :: (Int, Int) -> [Char]
+cardString :: (Int, Int) -> String
 cardString (cardRank, cardSuit) = head [[rankString, suitString] | 
-    (rank, rankString) <- rankStringMap, 
-    (suit, suitString) <- suitStringMap, 
-    rank == cardRank, 
-    suit == cardSuit]
+    (r, rankString) <- rankStringMap, 
+    (s, suitString) <- suitStringMap, 
+    r == cardRank, 
+    s == cardSuit]
 
+cardStrings :: [(Int, Int)] -> [String]
 cardStrings cards = [cardString(x) | x <- cards]
+
+fromCardString :: String -> (Int, Int)
+fromCardString [rankString, suitString] = head [(r, s) | 
+    (r, mappedRankString) <- rankStringMap,
+    (s, mappedSuitString) <- suitStringMap,
+    mappedRankString == rankString,
+    mappedSuitString == suitString]
+
+fromCardStrings :: [String] -> [(Int, Int)]
+fromCardStrings cardStringList = [fromCardString cardStringInstance | cardStringInstance <- cardStringList]
 
 --String Conversion Tests
 testCardString = cardString(5, 2) == "7H"
 testCardStrings = cardStrings [(0,0),(0,1)] == ["2C","2D"] && cardStrings deck == ["2C","2D","2H","2S","3C","3D","3H","3S","4C","4D","4H","4S","5C","5D","5H","5S","6C","6D","6H","6S","7C","7D","7H","7S","8C","8D","8H","8S","9C","9D","9H","9S","TC","TD","TH","TS","JC","JD","JH","JS","QC","QD","QH","QS","KC","KD","KH","KS","AC","AD","AH","AS"]
+testFromCardString = fromCardString "7S" == (5, 3)
+testFromCardStrings = fromCardStrings (cardStrings deck) == deck
 
-
+testAllStringConversions = testCardString && testCardStrings && testFromCardString && testFromCardStrings
